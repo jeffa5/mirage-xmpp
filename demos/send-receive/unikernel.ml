68e76388af4745e99fa3a79ce5e49fa2ac76ace9
TheAIAJ <theaiaj.general@gmail.com>
2018-10-22 22:28:24 +0100

    Further refactor send-receive
    Extracted the echo function from the code to make it more readable.

--- demos/send-receive/unikernel.ml
+++ demos/send-receive/unikernel.ml
@@ -7,21 +7,22 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     | Ok () -> Lwt.return_unit
     | Error e -> Logs.warn (fun f -> f "Error occurred from writing to connection: %a" S.TCPV4.pp_write_error e); Lwt.return_unit
 
+  let rec echo flow =
+    S.TCPV4.read flow >>= function
+    | Ok `Eof -> Logs.info (fun f -> f "Closing connection due to Eof!"); Lwt.return_unit
+    | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
+    | Ok (`Data b) ->
+      Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
+      let reply = (Cstruct.of_string ("echo: " ^ Cstruct.to_string b)) in
+      write_string flow reply >>= fun () ->
+      echo flow
+
   let on_connect flow =
     let dst, dst_port = S.TCPV4.dst flow in
     Logs.info (fun f -> f "new tcp connection from IP %s on port %d" (Ipaddr.V4.to_string dst) dst_port);
-    let message = Cstruct.of_string "*\n* Hello from Mirage!\n*\n" in
-    let rec loop () =
-      S.TCPV4.read flow >>= function
-      | Ok `Eof -> Logs.info (fun f -> f "Closing connection due to Eof!"); Lwt.return_unit
-      | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
-      | Ok (`Data b) ->
-        Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
-        let echo = (Cstruct.of_string ("echo: " ^ Cstruct.to_string b)) in
-        write_string flow echo >>= fun () ->
-        loop ()
-    in write_string flow message >>= fun () ->
-    loop () >>= fun () ->
+    let welcome = Cstruct.of_string "*\n* Hello from Mirage!\n*\n" in
+    write_string flow welcome >>= fun () ->
+    echo flow >>= fun () ->
     S.TCPV4.close flow
 
   let start s =
