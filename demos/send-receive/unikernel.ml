95dbae6b670e75feb69a21f4238f5e6f91c13b9b
TheAIAJ <theaiaj.general@gmail.com>
2018-10-22 22:14:51 +0100

    Add looping to send-receive demo
    The send-receive demo now accepts connections and will echo text back to
    clients in a loop indefinitely, or until the client
    closes the connection.
    
    Test updated to include showing off this feature

--- demos/send-receive/unikernel.ml
+++ demos/send-receive/unikernel.ml
@@ -1,26 +1,32 @@
 open Lwt.Infix
 
-
 module Main (S: Mirage_stack_lwt.V4) = struct
+
+  let write_string flow s =
+    S.TCPV4.write flow s >>= function
+    | Ok () -> Lwt.return_unit
+    | Error e -> Logs.warn (fun f -> f "Error occurred from writing to connection: %a" S.TCPV4.pp_write_error e); Lwt.return_unit
+
+  let on_connect flow =
+    let dst, dst_port = S.TCPV4.dst flow in
+    Logs.info (fun f -> f "new tcp connection from IP %s on port %d" (Ipaddr.V4.to_string dst) dst_port);
+    let message = Cstruct.of_string "*\n* Hello from Mirage!\n*\n" in
+    let rec loop () =
+      S.TCPV4.read flow >>= function
+      | Ok `Eof -> Logs.info (fun f -> f "Closing connection due to Eof!"); Lwt.return_unit
+      | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
+      | Ok (`Data b) ->
+        Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
+        let echo = (Cstruct.of_string ("echo: " ^ Cstruct.to_string b)) in
+        write_string flow echo >>= fun () ->
+        loop ()
+    in write_string flow message >>= fun () ->
+    loop () >>= fun () ->
+    S.TCPV4.close flow
 
   let start s =
     let port = Key_gen.port () in
-    S.listen_tcpv4 s ~port (fun flow ->
-        let dst, dst_port = S.TCPV4.dst flow in
-        (* Log the information of the incoming connection *)
-        Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
-                      (Ipaddr.V4.to_string dst) dst_port);
-        S.TCPV4.read flow >>= function
-        | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
-        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
-        | Ok (`Data b) ->
-          Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
-          let message = Cstruct.of_string "*\n* Hello from Mirage!\n*\n" in
-          S.TCPV4.write flow message >>= function
-          | Ok () -> S.TCPV4.close flow
-          | Error e -> Logs.warn (fun f -> f "Error occurred from writing to connection: %a" S.TCPV4.pp_write_error e); Lwt.return_unit
-      );
-
+    S.listen_tcpv4 s ~port on_connect;
     S.listen s
 
 end
