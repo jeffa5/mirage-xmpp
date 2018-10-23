b1daa06b1ff86341a945e536956677be2b88cd48
TheAIAJ <theaiaj.general@gmail.com>
2018-10-24 00:22:36 +0100

    Add new function to pass to the parser
    The new function xml_source allows the parser to be used over multiple
    messages sent by the client

--- demos/xml-parsing/unikernel.ml
+++ demos/xml-parsing/unikernel.ml
@@ -7,6 +7,8 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     | Ok () -> Lwt.return_unit
     | Error e -> Logs.warn (fun f -> f "Error occurred from writing to connection: %a" S.TCPV4.pp_write_error e); Lwt.return_unit
 
+  let attr_to_string = function
+    | ((uri, local), value) -> uri ^ ":" ^ local ^ "=" ^ value
 
   let parse_xml i =
     (* Need to catch errors thrown when invalid xml is received *)
@@ -14,6 +16,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
       match Xmlm.input i with
       | `El_start ((uri, local), attr_list) ->
         Logs.debug (fun f -> f "Found the start of an element: %s:%s" uri local);
+        List.iter (fun attr -> Logs.debug (fun f -> f "attribute: %s" (attr_to_string attr))) attr_list;
         pull i (depth + 1)
       | `El_end -> Logs.debug (fun f -> f "Found an end of an element at depth %d" depth);
         if depth = 1 then Lwt.return_unit else pull i (depth - 1)
@@ -29,6 +32,34 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     if not (Xmlm.eoi i) then Logs.warn (fun f -> f "Invalid xml received");
     Lwt.return_unit
 
+  let xml_source flow =
+    let index = ref 0 in
+    let bytes = ref None in
+    let rec read_byte () =
+      match !bytes with
+      | None -> (
+          match Lwt_main.run (S.TCPV4.read flow) with
+          | Ok `Eof -> raise End_of_file
+          | Error _ -> raise End_of_file
+          | Ok (`Data b) ->
+            index := 0;
+            bytes := Some (Cstruct.to_bytes b);
+            read_byte ()
+        )
+      | Some bs ->
+        if !index = Bytes.length bs then (
+          index := 0;
+          bytes := None;
+          read_byte ()
+        )
+        else (
+          let byte = Bytes.get bs !index in
+          index := !index + 1;
+          Char.code byte
+        )
+    in
+    read_byte
+
   let rec read_xml flow =
     S.TCPV4.read flow >>= function
     | Ok `Eof -> Logs.info (fun f -> f "Closing connection due to Eof!"); Lwt.return_unit
@@ -38,7 +69,8 @@ module Main (S: Mirage_stack_lwt.V4) = struct
     | Ok (`Data b) ->
       Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
       (* Need to create a channel which we can use to write the given string into *)
-      let i = Xmlm.make_input (`String (0, Cstruct.to_string b)) in
+      let i = Xmlm.make_input (`Fun (xml_source flow)) in
+      (* let i = Xmlm.make_input (`String (0, Cstruct.to_string b)) in *)
       parse_xml i >>= fun () ->
       read_xml flow
 
