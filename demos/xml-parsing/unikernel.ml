647bca5648d32ea0c076bd80b9c1574792699a17
TheAIAJ <theaiaj.general@gmail.com>
2018-10-26 12:38:35 +0100

    Add exception generation and handling to parser
    Parser handles some simple errors by generating exceptions.
    This means they can be handled when we actually do the parsing.
    Currently we simply send the error to the user.

--- demos/xml-parsing/unikernel.ml
+++ demos/xml-parsing/unikernel.ml
@@ -27,43 +27,54 @@ module Main (S : Mirage_stack_lwt.V4) = struct
     aux ()
   ;;
 
-  let parse_xml stream =
+  exception Bad_document of string
+  exception Unmatched_tag of string
+
+  let parse_xml flow stream =
     let stream = Markup_lwt.lwt_stream stream in
     let parser =
       Markup_lwt.parse_xml
-        ~report:(fun l e ->
+        ~report:(fun _ e ->
           Logs.warn (fun f ->
               f "Error occurred during parsing: %s" (Markup.Error.to_string e) );
-          Lwt.return_unit )
+          match e with
+          | `Bad_document reason -> raise (Bad_document reason)
+          | `Unmatched_end_tag tag | `Unmatched_start_tag tag ->
+            raise (Unmatched_tag tag)
+          | _ -> Lwt.return_unit )
         stream
     in
     let signals = Markup.signals parser in
     let rec pull_signal depth =
-      Markup_lwt.next signals
-      >>= function
-      | Some signal ->
-        (match signal with
-        | `Start_element ((uri, local), attrs) ->
-          Logs.debug (fun f -> f "Start element received: %s:%s" uri local);
-          pull_signal (depth + 1)
-        | `End_element ->
-          Logs.debug (fun f -> f "End element received");
-          if depth = 1 then Lwt.return_unit else pull_signal (depth - 1)
-        | `Text strings ->
-          let stripped_strings = List.map ~f:(fun s -> String.strip s) strings in
-          let non_empty_strings =
-            List.filter ~f:(fun s -> String.strip s <> "") stripped_strings
-          in
-          List.iter
-            ~f:(fun s -> Logs.debug (fun f -> f "Found text: %s" s))
-            non_empty_strings;
-          pull_signal depth
-        | _ ->
-          Logs.debug (fun f -> f "signal received! %s" (Markup.signal_to_string signal));
-          pull_signal depth)
-      | None ->
-        Logs.debug (fun f -> f "None signal received");
-        Lwt.return_unit
+      try
+        Markup_lwt.next signals
+        >>= function
+        | Some signal ->
+          (match signal with
+          | `Start_element ((uri, local), attrs) ->
+            Logs.debug (fun f -> f "Start element received: %s:%s" uri local);
+            pull_signal (depth + 1)
+          | `End_element ->
+            Logs.debug (fun f -> f "End element received");
+            if depth = 1 then Lwt.return_unit else pull_signal (depth - 1)
+          | `Text strings ->
+            let stripped_strings = List.map ~f:(fun s -> String.strip s) strings in
+            let non_empty_strings =
+              List.filter ~f:(fun s -> String.strip s <> "") stripped_strings
+            in
+            List.iter
+              ~f:(fun s -> Logs.debug (fun f -> f "Found text: %s" s))
+              non_empty_strings;
+            pull_signal depth
+          | _ ->
+            Logs.debug (fun f -> f "signal received! %s" (Markup.signal_to_string signal));
+            pull_signal depth)
+        | None ->
+          Logs.debug (fun f -> f "None signal received");
+          Lwt.return_unit
+      with
+      | Bad_document reason -> write_string flow ("Bad XML given: " ^ reason)
+      | Unmatched_tag tag -> write_string flow ("Unmatched tag found: " ^ tag)
     in
     pull_signal 0
   ;;
@@ -71,7 +82,7 @@ module Main (S : Mirage_stack_lwt.V4) = struct
   let on_connect flow =
     let stream, pushf = Lwt_stream.create () in
     Lwt.async (fun () -> read flow pushf);
-    parse_xml stream
+    parse_xml flow stream
     >>= fun () ->
     Logs.debug (fun f -> f "Closing the connection");
     S.TCPV4.close flow
