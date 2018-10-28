c0442450b398253193ac96cd846baa28803294fb
TheAIAJ <theaiaj.general@gmail.com>
2018-10-28 13:48:03 +0000

    Refactor parser file

--- demos/xml-parsing/src/parser.ml
+++ demos/xml-parsing/src/parser.ml
@@ -3,29 +3,26 @@ open Core_kernel
 module type Flowout = Flowout.Main
 
 module Main (F : Flowout) = struct
-  let parse_xml flow stream =
-    let stream = Markup_lwt.lwt_stream stream in
-    let parser_report = ref (Ok ()) in
-    let parser =
-      Markup_lwt.parse_xml
-        ~report:(fun _ e ->
-          let error_string = Markup.Error.to_string e in
-          Logs.warn (fun f -> f "Error occurred during parsing: %s" error_string);
-          match e with
-          | `Bad_document _ ->
-            parser_report := Error error_string;
-            Lwt.return_unit
-          | `Unmatched_end_tag _ | `Unmatched_start_tag _ ->
-            parser_report := Error error_string;
-            Lwt.return_unit
-          | _ -> Lwt.return_unit )
-        stream
-    in
-    let signals = Markup.signals parser in
-    Logs.info (fun f -> f "Setup parser and signals, beginning to pull signals.");
-    let rec pull_signal depth =
+  let make_parser parser_status stream =
+    Markup_lwt.parse_xml
+      ~report:(fun _ e ->
+        let error_string = Markup.Error.to_string e in
+        Logs.warn (fun f -> f "Error occurred during parsing: %s" error_string);
+        match e with
+        | `Bad_document _ ->
+          parser_status := Error error_string;
+          Lwt.return_unit
+        | `Unmatched_end_tag _ | `Unmatched_start_tag _ ->
+          parser_status := Error error_string;
+          Lwt.return_unit
+        | _ -> Lwt.return_unit )
+      stream
+  ;;
+
+  let pull_signal flow signals parser_status =
+    let rec aux depth =
       let%lwt signal = Markup_lwt.next signals in
-      match !parser_report with
+      match !parser_status with
       | Error s ->
         Logs.warn (fun f ->
             f "Parser report generated an error. Notifying user and exiting parsing." );
@@ -37,26 +34,35 @@ module Main (F : Flowout) = struct
           | `Start_element _ ->
             Logs.debug (fun f ->
                 f "Start element received: %s" (Markup.signal_to_string signal) );
-            pull_signal (depth + 1)
+            aux (depth + 1)
           | `End_element ->
             Logs.debug (fun f -> f "End element received");
             if depth = 1
             then (
               Logs.info (fun f -> f "Accepting the parsed XML and notifying user");
               F.write_string flow "XML accepted." )
-            else pull_signal (depth - 1)
+            else aux (depth - 1)
           | `Text _ ->
             let signal_string = String.strip (Markup.signal_to_string signal) in
             if signal_string <> ""
             then Logs.debug (fun f -> f "Text received: %s" signal_string);
-            pull_signal depth
+            aux depth
           | _ ->
             Logs.debug (fun f -> f "Signal received! %s" (Markup.signal_to_string signal));
-            pull_signal depth)
+            aux depth)
         | None ->
           Logs.debug (fun f -> f "None signal received");
           Lwt.return_unit)
     in
-    pull_signal 0
+    aux 0
+  ;;
+
+  let parse_xml flow stream =
+    let parser_status = ref (Ok ()) in
+    let signals =
+      Markup_lwt.lwt_stream stream |> make_parser parser_status |> Markup.signals
+    in
+    Logs.info (fun f -> f "Setup parser, beginning to pull signals.");
+    pull_signal flow signals parser_status
   ;;
 end
