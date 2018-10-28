92b9aa7fc7676388286f6b1e0c2e2cc02ebe5768
TheAIAJ <theaiaj.general@gmail.com>
2018-10-28 23:26:18 +0000

    Change testing to use async sockets
    We now use async sockets to communicate with the unikernel.
    This is opposed to making system calls to use nc to do the work.

--- demos/xml-parsing/test/test.ml
+++ demos/xml-parsing/test/test.ml
@@ -1,77 +1,89 @@
-open Core
+open Async
 
-let send ?(host = "10.0.0.2") ?(port = "8080") str =
-  let str = Sys.quote str in
-  let command = "echo " ^ str ^ " | nc -nw1 " ^ host ^ " " ^ port in
-  Sys.command_exn command
+let send ?(host = "10.0.0.2") ?(port = 8080) str =
+  let host_and_port = Core.Host_and_port.create ~host ~port in
+  let where_to_connect = Tcp.Where_to_connect.of_host_and_port host_and_port in
+  let%bind s =
+    Tcp.with_connection where_to_connect (fun _socket reader writer ->
+        Writer.write writer str;
+        Reader.contents reader )
+  in
+  printf "%s" s;
+  Deferred.unit
 ;;
 
 let%expect_test "no start tag" =
-  send "</error>";
+  let%bind () = send "</error>" in
   [%expect {| Error: bad document: expected root element |}]
 ;;
 
 let%expect_test "unmatched end tag" =
-  send "<start></a></start>";
+  let%bind () = send "<start></a></start>" in
   [%expect {| Error: unmatched end tag 'a' |}]
 ;;
 
 let%expect_test "unmatched start tag" =
-  send "<start><a></start>";
+  let%bind () = send "<start><a></start>" in
   [%expect {| Error: unmatched start tag 'a' |}]
 ;;
 
 let%expect_test "simple open close" =
-  send "<stream></stream>";
+  let%bind () = send "<stream></stream>" in
   [%expect {| XML accepted. |}]
 ;;
 
 let%expect_test "no closing tag" =
-  send "<message><body>No closing tag!</message>";
+  let%bind () = send "<message><body>No closing tag!</message>" in
   [%expect {| Error: unmatched start tag 'body' |}]
 ;;
 
 let%expect_test "xmpp initial" =
-  send
-    "<stream:stream\n\
-    \       from='juliet@im.example.com'\n\
-    \       to='im.example.com'\n\
-    \       version='1.0'\n\
-    \       xml:lang='en'\n\
-    \       xmlns='jabber:client'\n\
-    \       xmlns:stream='http://etherx.jabber.org/streams'>\n\
-    \     <message>\n\
-    \       <body>foo</body>\n\
-    \     </message>\n\
-    \   </stream:stream>";
+  let%bind () =
+    send
+      "<stream:stream\n\
+      \       from='juliet@im.example.com'\n\
+      \       to='im.example.com'\n\
+      \       version='1.0'\n\
+      \       xml:lang='en'\n\
+      \       xmlns='jabber:client'\n\
+      \       xmlns:stream='http://etherx.jabber.org/streams'>\n\
+      \     <message>\n\
+      \       <body>foo</body>\n\
+      \     </message>\n\
+      \   </stream:stream>"
+  in
   [%expect {| XML accepted. |}]
 ;;
 
 let%expect_test "unknown namespace" =
-  send
-    "<stream:error>\n\
-    \        <not-well-formed\n\
-    \            xmlns='urn:ietf:params:xml:ns:xmpp-streams'/>\n\
-    \      </stream:error>\n\
-    \      </stream:stream>";
+  let%bind () =
+    send
+      "<stream:error>\n\
+      \        <not-well-formed\n\
+      \            xmlns='urn:ietf:params:xml:ns:xmpp-streams'/>\n\
+      \      </stream:error>\n\
+      \      </stream:stream>"
+  in
   [%expect {| Error: unknown namespace 'stream' |}]
 ;;
 
 let%expect_test "xmpp initial extended" =
-  send
-    "<?xml version='1.0'?>\n\
-    \      <stream:stream\n\
-    \          from='im.example.com'\n\
-    \          id='++TR84Sm6A3hnt3Q065SnAbbk3Y='\n\
-    \          to='juliet@im.example.com'\n\
-    \          version='1.0'\n\
-    \          xml:lang='en'\n\
-    \          xmlns='jabber:client'\n\
-    \          xmlns:stream='http://etherx.jabber.org/streams'>\n\
-    \      <stream:error>\n\
-    \        <invalid-namespace\n\
-    \            xmlns='urn:ietf:params:xml:ns:xmpp-streams'/>\n\
-    \      </stream:error>\n\
-    \      </stream:stream>";
+  let%bind () =
+    send
+      "<?xml version='1.0'?>\n\
+      \      <stream:stream\n\
+      \          from='im.example.com'\n\
+      \          id='++TR84Sm6A3hnt3Q065SnAbbk3Y='\n\
+      \          to='juliet@im.example.com'\n\
+      \          version='1.0'\n\
+      \          xml:lang='en'\n\
+      \          xmlns='jabber:client'\n\
+      \          xmlns:stream='http://etherx.jabber.org/streams'>\n\
+      \      <stream:error>\n\
+      \        <invalid-namespace\n\
+      \            xmlns='urn:ietf:params:xml:ns:xmpp-streams'/>\n\
+      \      </stream:error>\n\
+      \      </stream:stream>"
+  in
   [%expect {| XML accepted. |}]
 ;;
