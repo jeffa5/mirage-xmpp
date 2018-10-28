dd1fe4cd397a6052c0a27a7056762c9413965abb
TheAIAJ <theaiaj.general@gmail.com>
2018-10-28 18:19:25 +0000

    Change parser error handling
    Error handling didn't correctly write to the user.
    The errors now generate exceptions which are handled
    regardless of waiting for the next signal to come out.
    
    This simplifies a lot of the code for the error handling and
    separates it from the rest more clearly.

--- demos/xml-parsing/src/parser.ml
+++ demos/xml-parsing/src/parser.ml
@@ -3,66 +3,55 @@ open Core_kernel
 module type Flowout = Flowout.Main
 
 module Main (F : Flowout) = struct
-  let make_parser parser_status stream =
+  exception ParsingError
+
+  let make_parser flow stream =
     Markup_lwt.parse_xml
       ~report:(fun _ e ->
         let error_string = Markup.Error.to_string e in
         Logs.warn (fun f -> f "Error occurred during parsing: %s" error_string);
-        match e with
-        | `Bad_document _ ->
-          parser_status := Error error_string;
-          Lwt.return_unit
-        | `Unmatched_end_tag _ | `Unmatched_start_tag _ ->
-          parser_status := Error error_string;
-          Lwt.return_unit
-        | _ -> Lwt.return_unit )
+        let%lwt () = F.write_error_string flow error_string in
+        raise ParsingError )
       stream
   ;;
 
-  let pull_signal flow signals parser_status =
+  let pull_signal flow signals =
     let rec aux depth =
-      let%lwt signal = Markup_lwt.next signals in
-      match !parser_status with
-      | Error s ->
-        Logs.warn (fun f ->
-            f "Parser report generated an error. Notifying user and exiting parsing." );
-        F.write_error_string flow s
-      | Ok _ ->
-        (match signal with
-        | Some signal ->
-          (match signal with
-          | `Start_element _ ->
-            Logs.debug (fun f ->
-                f "Start element received: %s" (Markup.signal_to_string signal) );
-            aux (depth + 1)
-          | `End_element ->
-            Logs.debug (fun f -> f "End element received");
-            if depth = 1
-            then (
-              Logs.info (fun f -> f "Accepting the parsed XML and notifying user");
-              F.write_string flow "XML accepted." )
-            else aux (depth - 1)
-          | `Text _ ->
-            let signal_string = String.strip (Markup.signal_to_string signal) in
-            if signal_string <> ""
-            then Logs.debug (fun f -> f "Text received: %s" signal_string);
-            aux depth
-          | _ ->
-            Logs.debug (fun f -> f "Signal received! %s" (Markup.signal_to_string signal));
-            aux depth)
-        | None ->
-          Logs.debug (fun f -> f "None signal received");
-          Lwt.return_unit)
+      try%lwt
+            match%lwt Markup_lwt.next signals with
+            | Some signal ->
+              (match signal with
+              | `Start_element _ ->
+                Logs.debug (fun f ->
+                    f "Start element received: %s" (Markup.signal_to_string signal) );
+                aux (depth + 1)
+              | `End_element ->
+                Logs.debug (fun f -> f "End element received");
+                if depth = 1
+                then (
+                  Logs.info (fun f -> f "Accepting the parsed XML and notifying user");
+                  F.write_string flow "XML accepted." )
+                else aux (depth - 1)
+              | `Text _ ->
+                let signal_string = String.strip (Markup.signal_to_string signal) in
+                if signal_string <> ""
+                then Logs.debug (fun f -> f "Text received: %s" signal_string);
+                aux depth
+              | _ ->
+                Logs.debug (fun f ->
+                    f "Signal received! %s" (Markup.signal_to_string signal) );
+                aux depth)
+            | None ->
+              Logs.debug (fun f -> f "None signal received");
+              Lwt.return_unit
+      with ParsingError -> Lwt.return_unit
     in
     aux 0
   ;;
 
   let parse_xml flow stream =
-    let parser_status = ref (Ok ()) in
-    let signals =
-      Markup_lwt.lwt_stream stream |> make_parser parser_status |> Markup.signals
-    in
+    let signals = Markup_lwt.lwt_stream stream |> make_parser flow |> Markup.signals in
     Logs.info (fun f -> f "Setup parser, beginning to pull signals.");
-    pull_signal flow signals parser_status
+    pull_signal flow signals
   ;;
 end
