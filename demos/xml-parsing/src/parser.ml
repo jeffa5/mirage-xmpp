cceedecd24b4c984029d58f788b63068c6c2c725
TheAIAJ <theaiaj.general@gmail.com>
2018-10-29 09:30:43 +0000

    Re-factor code to not have cyclic dependency
    The code with functors was a bit ugly.
    I've simplified it so that parsing now returns results which can carry
    the messages we need to send out, if any.
    
    This also means the parser module doesn't depend on mirage stack either
    which is nicer to have.
    Writing tests would be easier since it can just take a stream to parse
    from now.

--- demos/xml-parsing/src/parser.ml
+++ demos/xml-parsing/src/parser.ml
@@ -1,57 +1,50 @@
 open Core_kernel
 
-module type Flowout = Flowout.Main
+exception ParsingError of string
 
-module Main (F : Flowout) = struct
-  exception ParsingError
+let make_parser stream =
+  Markup_lwt.parse_xml
+    ~report:(fun _ e ->
+      let error_string = Markup.Error.to_string e in
+      Logs.warn (fun f -> f "Error occurred during parsing: %s" error_string);
+      Lwt.fail (ParsingError error_string) )
+    stream
+;;
 
-  let make_parser flow stream =
-    Markup_lwt.parse_xml
-      ~report:(fun _ e ->
-        let error_string = Markup.Error.to_string e in
-        Logs.warn (fun f -> f "Error occurred during parsing: %s" error_string);
-        let%lwt () = F.write_error_string flow error_string in
-        raise ParsingError )
-      stream
-  ;;
+let pull_signal signals =
+  let rec aux depth =
+    match%lwt Markup_lwt.next signals with
+    | exception ParsingError e -> Lwt.return_error e
+    | Some signal ->
+      (match signal with
+      | `Start_element _ ->
+        Logs.debug (fun f ->
+            f "Start element received: %s" (Markup.signal_to_string signal) );
+        aux (depth + 1)
+      | `End_element ->
+        Logs.debug (fun f -> f "End element received");
+        if depth = 1
+        then (
+          Logs.info (fun f -> f "Accepting the parsed XML and notifying user");
+          Lwt.return_ok (Some "XML accepted.") )
+        else aux (depth - 1)
+      | `Text _ ->
+        let signal_string = String.strip (Markup.signal_to_string signal) in
+        if signal_string <> ""
+        then Logs.debug (fun f -> f "Text received: %s" signal_string);
+        aux depth
+      | _ ->
+        Logs.debug (fun f -> f "Signal received! %s" (Markup.signal_to_string signal));
+        aux depth)
+    | None ->
+      Logs.debug (fun f -> f "None signal received");
+      Lwt.return_ok None
+  in
+  aux 0
+;;
 
-  let pull_signal flow signals =
-    let rec aux depth =
-      try%lwt
-            match%lwt Markup_lwt.next signals with
-            | Some signal ->
-              (match signal with
-              | `Start_element _ ->
-                Logs.debug (fun f ->
-                    f "Start element received: %s" (Markup.signal_to_string signal) );
-                aux (depth + 1)
-              | `End_element ->
-                Logs.debug (fun f -> f "End element received");
-                if depth = 1
-                then (
-                  Logs.info (fun f -> f "Accepting the parsed XML and notifying user");
-                  F.write_string flow "XML accepted." )
-                else aux (depth - 1)
-              | `Text _ ->
-                let signal_string = String.strip (Markup.signal_to_string signal) in
-                if signal_string <> ""
-                then Logs.debug (fun f -> f "Text received: %s" signal_string);
-                aux depth
-              | _ ->
-                Logs.debug (fun f ->
-                    f "Signal received! %s" (Markup.signal_to_string signal) );
-                aux depth)
-            | None ->
-              Logs.debug (fun f -> f "None signal received");
-              Lwt.return_unit
-      with ParsingError -> Lwt.return_unit
-    in
-    aux 0
-  ;;
-
-  let parse_xml flow stream =
-    let signals = Markup_lwt.lwt_stream stream |> make_parser flow |> Markup.signals in
-    Logs.info (fun f -> f "Setup parser, beginning to pull signals.");
-    pull_signal flow signals
-  ;;
-end
+let parse_xml stream =
+  let signals = Markup_lwt.lwt_stream stream |> make_parser |> Markup.signals in
+  Logs.info (fun f -> f "Setup parser, beginning to pull signals.");
+  pull_signal signals
+;;
--- demos/xml-parsing/src/unikernel.ml
+++ demos/xml-parsing/src/unikernel.ml
@@ -1,8 +1,21 @@
 open Core_kernel
 
 module Main (S : Mirage_stack_lwt.V4) = struct
-  module Flowout = Flowout.Main (S)
-  module Xml = Parser.Main (Flowout)
+  let write_string flow s =
+    let s = String.strip s ^ "\n" in
+    let b = Cstruct.of_string s in
+    match%lwt S.TCPV4.write flow b with
+    | Ok () -> Lwt.return_unit
+    | Error e ->
+      Logs.warn (fun f ->
+          f "Error occurred from writing to connection: %a" S.TCPV4.pp_write_error e );
+      Lwt.return_unit
+  ;;
+
+  let write_error_string flow s =
+    let s = "Error: " ^ String.strip s in
+    write_string flow s
+  ;;
 
   let read flow pushf =
     let rec aux () =
@@ -10,6 +23,7 @@ module Main (S : Mirage_stack_lwt.V4) = struct
       | Ok `Eof | Error _ -> Lwt.return_unit
       | Ok (`Data b) ->
         let s = Cstruct.to_string b in
+        Logs.info (fun f -> f "Data read from connection: %s" s);
         String.iter ~f:(fun c -> pushf (Some c)) s;
         aux ()
     in
@@ -19,7 +33,12 @@ module Main (S : Mirage_stack_lwt.V4) = struct
   let on_connect flow =
     let stream, pushf = Lwt_stream.create () in
     Lwt.async (fun () -> read flow pushf);
-    let%lwt () = Xml.parse_xml flow stream in
+    let%lwt () =
+      match%lwt Parser.parse_xml stream with
+      | Ok (Some s) -> write_string flow s
+      | Ok None -> Lwt.return_unit
+      | Error s -> write_error_string flow s
+    in
     Logs.info (fun f -> f "Closing the connection");
     S.TCPV4.close flow
   ;;
