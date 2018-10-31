18021f71b2267fd1d1279619acb25d97fb0e28f2
TheAIAJ <theaiaj.general@gmail.com>
2018-10-31 23:03:41 +0000

    Format mli files too

--- ocamlformat.sh
+++ ocamlformat.sh
@@ -4,3 +4,8 @@ FILES=$(find . -name "*.ml" -type f)
 for f in $FILES; do
     ocamlformat --ocp-indent-compat --inplace --profile=janestreet $f
 done
+
+FILES=$(find . -name "*.mli" -type f)
+for f in $FILES; do
+    ocamlformat --ocp-indent-compat --inplace --profile=janestreet $f
+done
