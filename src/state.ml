5afa39075ec521334acda6828e077d89d797a322
TheAIAJ <theaiaj.general@gmail.com>
2018-11-08 00:28:05 +0000

    Update to new events and actions
    FSM now uses the new events and actions and properly replies to the
    first message from the client!

--- src/state.ml
+++ src/state.ml
@@ -1,4 +1,6 @@
 (* The state representing the current status of the connection *)
+open Stanza
+
 type state =
   | IDLE
   | OPENING
@@ -11,16 +13,18 @@ let to_string {state} = state_to_string state
 
 let update t event =
   let open Events in
-  match event with
-  | INITIAL s ->
+  match event with STANZA s ->
     (match t with
-    | {state = IDLE} -> {state = OPENING}, [Actions.SEND_STANZA s]
+    | {state = IDLE} ->
+      (match s with
+      | Stanza (((_, "stream"), attrs), []) ->
+        (* stream header stanza received, send own stream header *)
+        let from = get_value (get_attribute_by_name attrs "to") in
+        let dest = get_value (get_attribute_by_name attrs "from") in
+        let header = stream_header from dest in
+        {state = OPENING}, [Actions.REPLY_STANZA (false, header)]
+      | _ -> assert false)
     | _ -> assert false)
-  | STARTING -> t, []
-  | CLOSING ->
-    (match t with
-    | {state = IDLE} -> {state = OPENING}, []
-    | {state = OPENING} -> {state = IDLE}, [])
 ;;
 
 let%expect_test "create" =
@@ -31,11 +35,25 @@ let%expect_test "create" =
 
 let%expect_test "idle to opening" =
   let fsm = create () in
-  let stanza = Stanza.create (("", "test"), []) in
-  let fsm, actions = update fsm (Events.INITIAL stanza) in
+  let stanza =
+    Stanza.create
+      ( ("stream", "stream")
+      , [("", "from"), "juliet@im.example.com"; ("", "to"), "im.example.com"] )
+  in
+  let fsm, actions = update fsm (Events.STANZA stanza) in
   print_endline (to_string fsm);
   [%expect {| opening |}];
   let strings = List.map (fun a -> Actions.to_string a) actions in
-  List.iter (Printf.printf "%s ") strings;
-  [%expect {| send_xml |}]
+  List.iter (Printf.printf "%s\n") strings;
+  [%expect
+    {|
+    reply_stanza:
+    <stream:stream
+      from='im.example.com'
+      id=''
+      to='juliet@im.example.com'
+      version='1.0'
+      xml:lang='en'
+      xmlns='jabber:client'
+      xmlns:stream='http://etherx.jabber.org/streams'> |}]
 ;;
