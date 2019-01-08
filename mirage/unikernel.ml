fdf134eb725e4e3d2df05c28c85f6ee08d64fa76
Jeffa5 <theaiaj.general@gmail.com>
2019-01-08 14:47:41 +0000

    Remove write error string from unikernel

--- mirage/unikernel.ml
+++ mirage/unikernel.ml
@@ -10,18 +10,13 @@ module Main (S : Mirage_stack_lwt.V4) = struct
       Lwt.return_unit
   ;;
 
-  let write_error_string flow s =
-    let s = "Error: " ^ String.trim s in
-    write_string flow s
-  ;;
-
   let read flow pushf =
     let rec aux () =
       match%lwt S.TCPV4.read flow with
       | Ok `Eof | Error _ -> Lwt.return_unit
       | Ok (`Data b) ->
         let s = Cstruct.to_string b in
-        Logs.info (fun f -> f "Data read from connection: %s" s);
+        Logs.debug (fun f -> f "Data read from connection: %s" s);
         String.iter (fun c -> pushf (Some c)) s;
         aux ()
     in
