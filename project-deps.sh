eab41cecd08f0bd7cad7678b36c460a0892031bb
Jeffa5 <theaiaj.general@gmail.com>
2019-01-28 23:40:46 +0000

    Update project deps script

--- project-deps.sh
+++ project-deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "mirage $(dune external-lib-deps @all | sed '/\./d' | awk 'NR > 1 { printf "%s ",$2 }')"
+echo "mirage $(dune external-lib-deps @all | sed 's/\..*//' | uniq | awk 'NR > 1 { printf "%s ",$2 }')"
